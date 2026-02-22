a3f031b0a8d85127c5e89cabea51e411e05ca036
Sylvain <git@sylvain.pro>
2026-02-22 17:16:05 +0100

    Commits rework (#2)
    * BUILD: Add old portfolio to ashes/
    
    * BUILD: Add .gitignore
    
    * BUILD: Add Next.js package
    
    * BUILD: Add Next.js configuration
    
    * MAJOR: data: Add projects.json and veille.json
    
    * MAJOR: data: Added projects data files
    
    * MAJOR: types: Add data types declarations
    
    * MAJOR: components: Add UI components
    
    * MAJOR: components: Add terminal components
    
    * MAJOR: components: Add veille components
    
    * MAJOR: components: Add providers components
    
    * MAJOR: app: Add pages and routes
    
    * MEDIUM: ui: Add public assets
    
    * BUILD: Add format script
    
    * BUILD: Add automatic veille scripts
    
    * BUILD: Add deploy.yml

---
+++ src/types/data.d.ts
@@ -0,0 +1,56 @@
+// TypeScript declarations for JSON data imports
+// veille.json and projects.json should be located in src/data/
+declare module "@/data/veille.json" {
+    interface Article {
+        date: string;
+        title: string;
+        image: string;
+        link: string;
+        tags: string[];
+        description: string;
+        source: string;
+    }
+
+    interface Prerequis {
+        title: string;
+        fonctionnement: string;
+        protocole: string;
+        environnement: string;
+        link: string;
+    }
+
+    interface Veille {
+        title: string;
+        "sub-title": string;
+        definition: string;
+        fonctionnement: string;
+        prerequis: Prerequis[];
+        articles: Article[];
+    }
+
+    interface VeilleData {
+        veilles: Veille[];
+    }
+
+    const value: VeilleData;
+    export default value;
+}
+
+declare module "@/data/projects.json" {
+    interface Project {
+        id: number;
+        title: string;
+        tag: string;
+        description: string;
+        source: string;
+        download: string;
+        languages: string[];
+    }
+
+    interface ProjectsData {
+        projects: Project[];
+    }
+
+    const value: ProjectsData;
+    export default value;
+}
